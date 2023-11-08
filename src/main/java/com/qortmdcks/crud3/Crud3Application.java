0d761e6f6740333086722ee002ccfe63000f10f3
baekseungchan1117 <bsc7386@gmail.com>
2023-11-08 13:14:14 +0900

    Bean 설정

--- src/main/java/com/qortmdcks/crud3/Crud3Application.java
+++ src/main/java/com/qortmdcks/crud3/Crud3Application.java
@@ -1,10 +1,17 @@
 package com.qortmdcks.crud3;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Crud3Application {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Crud3Application.class, args);
